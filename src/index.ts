80ac749803eac03c50ecbbe5edd8b8a14db7dd29
Kantha <cforcloud@gmail.com>
2026-02-21 13:22:59 +0530

    chore: improve transformIndexHtml and types

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,57 @@
-import type { Plugin } from "vite";
+import type { HtmlTagDescriptor, IndexHtmlTransform, Plugin } from "vite";
 
 /// types
-export interface HtmlPagesOptions {}
+export type HtmlItem = {
+  /** unique page path */
+  path: string;
 
-/// consts
+  /** unique destination file */
+  filename: `${string}.html`;
+
+  tags?: HtmlTagDescriptor[];
+};
+
+type HtmlBaseOptions = {
+  /** @default 'node_modules' */
+  cacheParentDir?: string;
+
+  /** where temp html files are placed @default '.html-pages' */
+  cacheDir?: string;
+};
+
+export type HtmlPagesOptions = HtmlBaseOptions & {
+  pages?: HtmlItem[];
+};
+
+/// constants
 export const PLUGIN_NAME = "vite-plugin-html-pages";
 
 /// plugin
-export function viteHtmlPages(opts: HtmlPagesOptions = {}): Plugin {
-  return {
-    name: PLUGIN_NAME,
+export function viteHtmlPages(options: HtmlPagesOptions = {}): Plugin[] {
+  const opts = options;
 
-    transformIndexHtml(html) {
-      console.log("viteHtmlPages", opts);
+  const getTransformIndexHtml = (isBuild = false): IndexHtmlTransform => {
+    console.log("h:IndexHtmlTransform", { opts, isBuild });
 
-      return html.replace(/<title>(.*?)<\/title>/, `<title>Title replaced!</title>`);
-    },
+    return {
+      order: "pre",
+      handler(html, ctx) {
+        /// path, filename for build, since its virtual-like page
+        /// originalUrl is defined only in serve
+        const { path, filename, originalUrl } = ctx;
+
+        console.log("h:IndexHtmlTransform", { path, filename, originalUrl });
+
+        return html.replace(/<title>(.*?)<\/title>/, "<title>$1 Title replaced!</title>");
+      },
+    };
   };
-}
 
-export function test() {
-  return "works!";
+  const servePlugin: Plugin = {
+    name: PLUGIN_NAME,
+    apply: "serve",
+    transformIndexHtml: getTransformIndexHtml(),
+  };
+
+  return [servePlugin];
 }
