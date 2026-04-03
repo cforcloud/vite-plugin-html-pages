086d6d9035d97dac357675efa8c88beb473f7d08
Kantha <cforcloud@gmail.com>
2026-04-03 15:10:36 +0530

    docs(playground): update multi page config

--- playground/multi-page/src/main.ts
+++ playground/multi-page/src/main.ts
@@ -4,9 +4,15 @@ import "./style.css";
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <h1>multi-page!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
+    <button id="counter" type="button"></button>
+
+    <nav>
+      <ol>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ol>
+    </nav>
   </div>
 `;
 
